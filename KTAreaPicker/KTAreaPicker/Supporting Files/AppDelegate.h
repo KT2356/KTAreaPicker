8af5d75e94847e1ae1f2f35fc5dccbc7bbaed84d
KT2356 <15152112356@163.com>
2015-11-25 18:06:04 +0800

    add files

---
+++ KTAreaPicker/KTAreaPicker/Supporting Files/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KTAreaPicker
+//
+//  Created by KT on 15/11/25.
+//  Copyright © 2015年 KT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
